f18a1727712f4440bf1b39deb55053c7e3d5fd79
Polvory <32514592+Polvory@users.noreply.github.com>
2020-06-06 12:56:28 +0300

    Add files via upload

---
+++ src/src/src/components/P.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Testp = ()=>{
+
+
+    return (
+
+        <>
+        <p>текст</p>
+        <button className="FirstAccentButton" >close</button>
+        </>
+    )
+}
+
+export default Testp
